629ac3223607d247f939d889f47001e8fb6574d7
brandonn-t <brandon.torres619@gmail.com>
2022-08-11 14:09:21 -0400

    posting to DB works.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyparser.json());
+
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
+const postsRoute = require('./routes/posts');
+app.use('/posts', postsRoute);
 
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser: true} ,() => console.log("connected to DB!"));
 
 
 app.listen(3000);
